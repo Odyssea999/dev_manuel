277a55ef75d1cb931eed03f582db52be07ea8cb3
elimf <90334513+elimf@users.noreply.github.com>
2024-11-10 18:36:48 +0100

    Enhance team member profiles and improve styling for better user experience

--- website/src/components/Team.js
+++ website/src/components/Team.js
@@ -7,24 +7,46 @@ const teamMembers = [
     name: 'Florvil Elim',
     role: 'Développeur full-stack',
     email: 'elim.florvil@edu.esiee-it.fr',
-    bio: 'Elim est un expert en développement web et a travaillé sur de nombreux projets innovants.',
+    bio: 'Elim est un développeur full-stack expérimenté, spécialisé dans le développement web. Curieux et créatif, il aime explorer les technologies modernes pour concevoir des applications dynamiques et robustes.',
     imgSrc: '/img/team/elim.jpeg',
   },
   {
-    name: 'Cohen-Joly Florian',
-    role: 'Développeur mobile',
-    email: 'florian.cohen-joly@edu.esiee-it.fr',
-    bio: 'Florian est un développeur mobile passionné par les nouvelles technologies. Il a travaillé sur de nombreux projets mobiles innovants.',
-    imgSrc: '/img/team/flo.png',
+    name: 'NSINGI Moïse',
+    role: 'Développeur full-stack',
+    email: 'moise.nsingi@edu.esiee-it.fr',
+    bio: 'Moïse est passionné par le développement mobile et toujours à la recherche de nouvelles façons d’améliorer l’expérience utilisateur. Sa maîtrise des technologies modernes fait de lui un atout précieux pour tout projet mobile.',
+    imgSrc: '/img/team/dev.png',
+  },
+  {
+    name: 'MUZAFFAR Eddy',
+    role: 'Développeur full-stack',
+    email: 'eddy.muzaffar@edu.esiee-it.fr',
+    bio: 'Eddy est un développeur full-stack avec une expertise en applications mobiles. Sa passion pour l’innovation le pousse à créer des solutions intuitives, centrées sur les utilisateurs et optimisées pour la performance.',
+    imgSrc: '/img/team/dev.png',
   },
   {
     name: 'Adje Abed',
-    role: 'Développeur mobile',
+    role: 'Développeur full-stack',
     email: 'abed.adje@edu.esiee-it.fr',
-    bio: 'Abed est un développeur mobile passionné par les nouvelles technologies. Il a travaillé sur de nombreux projets mobiles innovants.',
+    bio: 'Abed est un développeur polyvalent, adepte des dernières technologies en matière de développement mobile. Son approche analytique et son souci du détail lui permettent de créer des applications performantes et fiables.',
     imgSrc: '/img/team/abed.jpeg',
   },
+  {
+    name: 'AZLOUK Wessim',
+    role: 'Développeur full-stack',
+    email: 'wessim.azlouk@edu.esiee-it.fr',
+    bio: 'Wessim est un développeur passionné par la technologie et la création de solutions numériques élégantes. Sa spécialité en développement mobile lui permet de transformer des idées novatrices en applications fonctionnelles et engageantes.',
+    imgSrc: '/img/team/dev.png',
+  },
+  {
+    name: 'BALAKRISHNAN Jeremy',
+    role: 'Développeur full-stack',
+    email: 'jeremy.balakrishnan@edu.esiee-it.fr',
+    bio: 'Jeremy est un développeur dévoué avec un intérêt marqué pour le développement mobile et les solutions digitales. Toujours à la pointe de la technologie, il s’engage à offrir des expériences utilisateur mémorables et des produits de haute qualité.',
+    imgSrc: '/img/team/dev.png',
+  },
 ];
+
 
 const Team = () => {
   return (
